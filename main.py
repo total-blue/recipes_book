2b5c57a0e99811b9d60a00037e7a499e9a511304
levinski <polinaprikhodova@gmail.com>
2020-01-29 21:46:49 +0300

    fixed

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
-#1
-with open('recipes.txt') as f:
-    cook_book = {}
-    keys = ('ingredient_name', 'quantity', 'measure')
-    i = iter(f)
-    try:
-        while True:
-            name, amount = next(i).rstrip(), next(i)
-            cook_book[name] = []
-            for line in range(int(amount)):
-                cook_book[name].append(dict(zip(keys, (next(i).rstrip().split('|')))))
-            next(i)
-    except StopIteration:
-        pass
+file = open('recipes.txt')
+keys = ['ingredient_name', 'quantity', 'measure']
+cook_book = {}
+ings = []
 
-#2
-with open('recipes.txt') as f:
-    def get_shop_list_by_dish(dishes, person_count):
-        cook_book = f.read().split('\n')
-        keys = ['quantity', 'measure']
-        rec_dict = {}
-        dish_inds = []
-        for dish in dishes:
-            for line in cook_book:
-                if line == dish:
-                    dish_inds.append(cook_book.index(line))
-        for dish_ind in dish_inds:
-            ret = int(cook_book[dish_ind+1])
-            ing_list = cook_book[dish_ind+2:dish_ind+2+ret]
-            for ing in ing_list:
-                x = ing.split('|')
-                rec_dict[x[0]] = dict(zip(keys, (int(x[1]) * person_count, x[2])))
-        if not rec_dict == {}:
-            print(rec_dict)
+while True:
+    dish_name = file.readline().strip()
+    ret = file.readline()
+    for i in range(int(ret)):
+        ing = dict(zip(keys,(file.readline().strip().split(' | '))))
+        ing['quantity'] = int(ing['quantity'])
+        ings.append(ing)
+    if not file.readline(): break
+    cook_book[dish_name] = ings
+    ings = []
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_dict = {}
+    for dish in set(dishes):
+        if not dish in cook_book:
+            print(f'no dish named {dish}')
         else:
-            print('There are no your dishes')
+            mult = dishes.count(dish)
+            dish_ings = cook_book.get(dish)
+            for ing in dish_ings:
+                ing['quantity'] = int(ing['quantity']) * person_count * mult
+                shop_dict[ing.pop('ingredient_name')] = ing
+            print(shop_dict)
